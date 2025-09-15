98543547e9638ffe489a9e99ba81033005ec334c
janka <127381689+ArtisanJanka@users.noreply.github.com>
2025-09-15 09:32:28 +0300

    create

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,6 +1,21 @@
 <x-layout>
     <x-slot:title>
         Quiz SignIn
+
     </x-slot:title>
+
+        <div id="SignUp">
+        <h1>SignUp</h1>
+        <form action="/SignUp" method="POST">
+            @csrf
+            <input type="text" name="username" placeholder="UserName">
+            <input type="password" name="password" placeholder="Password">
+
+            <div class="button">
+                <input type="submit" value="SignUp">
+                <input type="button" onclick="location.href='/Login'" value="Log in">
+            </div>
+        </form>
+    </div>
 
 </x-latout>
