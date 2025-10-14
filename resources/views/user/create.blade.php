b2fac20667f1ee0a85ba93ede3a882aa08c217fb
janka <127381689+ArtisanJanka@users.noreply.github.com>
2025-10-14 11:25:45 +0300

    design

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -4,18 +4,21 @@
 
     </x-slot:title>
 
-        <div id="SignUp">
+        <div id="SignUp" class="logcontainer">
+            <div class="loginform">
         <h1>SignUp</h1>
+    
         <form action="/SignUp" method="POST">
             @csrf
-            <input type="text" name="username" placeholder="UserName">
-            <input type="password" name="password" placeholder="Password">
+            <input type="text" name="username" placeholder="UserName" class="input">
+            <input type="password" name="password" placeholder="Password" class="input">
 
             <div class="button">
-                <input type="submit" value="SignUp">
-                <input type="button" onclick="location.href='/Login'" value="Log in">
+                <input type="submit" value="SignUp" class="button1">
+                <input type="button" onclick="location.href='/Login'" value="Log in" class="button1">
             </div>
         </form>
+        </div>
     </div>
 
 </x-latout>
